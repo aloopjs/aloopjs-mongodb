05a4470f32571b00faa475eb8df272270ddf969a
Pencil <pencil@darasa.io>
2023-04-22 16:31:31 +0700

    Update setting mongo

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,4 +1,5 @@
 module.exports = {
   conn: process.env.MONGODB,
+  strict: false,
   options: {}
 };
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 module.exports = {
   register(){
     global.Aloop.models = () => require('./reader/model');
+    mongoose.set('strictQuery', global.Aloop.config.mongodb.strict || false);
     mongoose.connect(global.Aloop.config.mongodb.conn, global.Aloop.config.mongodb.options || {});
   },
   boot({app}) {
